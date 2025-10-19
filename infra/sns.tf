972026f5e637a12ff70bd38c83cab951b39f45a5
CristinaPineda <cristinadaspineda@yahoo.com.br>
2025-10-19 17:32:03 -0300

    :star: adiciona criação de logs no sns

--- infra/sns.tf
+++ infra/sns.tf
@@ -7,3 +7,42 @@ resource "aws_sns_topic" "rocket_project_sns_topic" {
     Environment = var.environment
   }
 }
+
+# Recurso 1: IAM Role que o SNS assumirá
+resource "aws_iam_role" "sns_delivery_logging_role" {
+  name = "sns-delivery-logging-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "sns.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Recurso 2: IAM Policy que permite o SNS escrever no CloudWatch Logs
+resource "aws_iam_role_policy" "sns_delivery_logging_policy" {
+  name = "sns-delivery-logging-policy"
+  role = aws_iam_role.sns_delivery_logging_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:log-group:/aws/sns/*"
+      }
+    ]
+  })
+}
